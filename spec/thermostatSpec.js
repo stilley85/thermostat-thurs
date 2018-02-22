a93da25d47035019120dfadf8a24482b730cb9e5
jose <jennymarin1989@hotmail.com>
2018-02-22 12:43:46 +0000

    Setting the temperature to minimum and raising error

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -13,6 +13,25 @@ describe('Thermostat', function(){
       thermostat.upTemperature()
       expect(thermostat.getCurrentTemperature()).toEqual(21);
     });
+    it('decreases the temperature', function(){
+      thermostat.down()
+      expect(thermostat.getCurrentTemperature()).toEqual(19);
+    });
+  });
+
+  describe('is minimum', function(){
+    it('has a minimum temperature of 10 degrees', function(){
+      for (var i = 1; i < 11; i++ ){
+        thermostat.down();
+      }
+      expect(function(){thermostat.down();}).toThrow(new Error("Minimum temperature of 10 degrees"));
+    });
+
+    it('is minimum returns true if temperature is 10', function(){
+      expect(thermostat.isMinimum(10)).toEqual(true);
+
+    });
 
   });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -4,10 +4,29 @@ var Thermostat = function(){
   this.temperature = 20;
 };
 
+  const MIN_TEMP = 10;
+
 Thermostat.prototype.getCurrentTemperature = function(){
   return this.temperature;
 };
 
 Thermostat.prototype.upTemperature = function(){
   this.temperature +=1;
-}
+};
+
+Thermostat.prototype.down = function(){
+  if(this.isMinimum(this.temperature)){
+    throw new Error("Minimum temperature of 10 degrees")
+  } else {
+    this.temperature -= 1;
+  }
+};
+
+Thermostat.prototype.isMinimum = function(temperature){
+  if(temperature === MIN_TEMP){
+    return true;
+  } else {
+    return false;
+  }
+
+};
